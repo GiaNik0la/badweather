6d2999d68f00261f464a2ede9877b53d5f4a1418
GiaNik0la <gianikolishvili0804@gmail.com>
2024-12-22 19:24:20 +0400

    weather api

---
+++ build/CMakeFiles/weather.dir/link.d
@@ -0,0 +1,79 @@
+weather: \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/libnghttp3.so.9 \
+  /usr/lib/libnghttp2.so.14 \
+  /usr/lib/libidn2.so.0 \
+  /usr/lib/libssh2.so.1 \
+  /usr/lib/libpsl.so.5 \
+  /usr/lib/libssl.so.3 \
+  /usr/lib/libcrypto.so.3 \
+  /usr/lib/libgssapi_krb5.so.2 \
+  /usr/lib/libzstd.so.1 \
+  /usr/lib/libbrotlidec.so.1 \
+  /usr/lib/libz.so.1 \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/lib/libunistring.so.5 \
+  /usr/lib/libkrb5.so.3 \
+  /usr/lib/libk5crypto.so.3 \
+  /usr/lib/libcom_err.so.2 \
+  /usr/lib/libkrb5support.so.0 \
+  /usr/lib/libkeyutils.so.1 \
+  /usr/lib/libresolv.so.2 \
+  /usr/lib/libbrotlicommon.so.1
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libc.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/libnghttp3.so.9:
+
+/usr/lib/libnghttp2.so.14:
+
+/usr/lib/libidn2.so.0:
+
+/usr/lib/libssh2.so.1:
+
+/usr/lib/libpsl.so.5:
+
+/usr/lib/libssl.so.3:
+
+/usr/lib/libcrypto.so.3:
+
+/usr/lib/libgssapi_krb5.so.2:
+
+/usr/lib/libzstd.so.1:
+
+/usr/lib/libbrotlidec.so.1:
+
+/usr/lib/libz.so.1:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/lib/libunistring.so.5:
+
+/usr/lib/libkrb5.so.3:
+
+/usr/lib/libk5crypto.so.3:
+
+/usr/lib/libcom_err.so.2:
+
+/usr/lib/libkrb5support.so.0:
+
+/usr/lib/libkeyutils.so.1:
+
+/usr/lib/libresolv.so.2:
+
+/usr/lib/libbrotlicommon.so.1:
---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <curl/curl.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+	if (argc != 3) {
+		fprintf(stderr, "not enough arguments\n");
+		return 1;
+	}
+
+	const char* API_KEY = "";
+	char* url = malloc(sizeof(char) * 100);
+
+	snprintf(url, 100, "http://api.weatherapi.com/v1/current.json?key=%s&q=%s,%s&aqi=no",
+			API_KEY,
+			argv[1], argv[2]);
+
+	CURL* curl = curl_easy_init();
+	CURLcode res;
+
+	if (!curl) {
+		fprintf(stderr, "curl could not be initialized\n");
+		return 1;
+	}
+
+	res = curl_easy_setopt(curl, CURLOPT_URL, url);
+
+	if (res != CURLE_OK) {
+		fprintf(stderr, "%s\n", curl_easy_strerror(res));
+		return 1;
+	}
+
+	res = curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+	if (res != CURLE_OK) {
+		fprintf(stderr, "%s\n", curl_easy_strerror(res));
+		return 1;
+	}
+
+	res = curl_easy_perform(curl);
+	
+	if (res != CURLE_OK) {
+		fprintf(stderr, "%s\n", curl_easy_strerror(res));
+		return 1;
+	}
+
+	printf("\n");
+
+	curl_easy_cleanup(curl);
+	return 0;
+}
